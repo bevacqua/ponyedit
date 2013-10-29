b05364398d24a7acd1d862788df6d6d405e76568
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2013-10-29 19:37:41 -0300

    initial commit

---
+++ src/ponyedit.js
@@ -0,0 +1,340 @@
+(function(window, document, ee) {
+	'use strict';
+
+	var ponies = [];
+	var wasCollapsed;
+	var query = document.queryCommandValue.bind(document);
+	var exec = document.execCommand.bind(document);
+	var addEventListener = 'addEventListener';
+
+	function find (element) {
+		var i = 0;
+		var len = ponies.length;
+		var item;
+		for(; i < len; i++) {
+			item = ponies[i];
+			if(item.element === element) {
+				return item.editor;
+			}
+		}
+	}
+
+	function Editor (element, options) {
+		var self = this;
+
+		ee.call(self, {
+			wildcard: true // support wildcard listeners
+		});
+
+		self.content = element;
+		self.options = options || {};
+		bindElements.call(self);
+		buildPastebin.call(self);
+		createEventBindings.call(self);
+
+		ponies.push({ editor: self, element: element });
+	}
+
+	// extends EventEmitter2
+	Editor.prototype = Object.create(ee.prototype);
+	Editor.prototype.constructor = Editor;
+
+	function bindElements () {
+		var self = this;
+		var content = self.content;
+
+		content.contentEditable = true;
+		content.classList.add('py-editable');
+	};
+
+	function buildPastebin () {
+		var self = this;
+		var pastebin = document.createElement('textarea');
+		pastebin.className = 'py-pastebin';
+		self.pastebin = pastebin;
+
+		document.body.appendChild(pastebin);
+		document[addEventListener]('paste', pasteHandler.bind(self));
+		document[addEventListener]('keydown', convertTabs.bind(self));
+		document[addEventListener]('keypress', function (e) {
+
+			if (((	(e.metaKey && !e.ctrlKey) ||
+					(e.ctrlKey && !e.metaKey)) && e.keyCode === 118)
+				/* || (e.shiftKey && INSERT)*/) { // command-v, ctrl-v, shift-ins
+				pasteHandler(e);
+			}
+		});
+	};
+
+	function pasteHandler (e) {
+		var self = this;
+		if (!isChildOf(self.content, e.target)) { return; }
+
+		self.saveSelection();
+		pastebin.focus();
+
+		setTimeout(function () {
+			self.restoreSelection(true);
+			insertTextAtCaret(pastebin.value);
+			pastebin.value = '';
+		});
+	}
+
+	function convertTabs (e) {
+		var self = this;
+		if (!isChildOf(self.content, e.target)) { return; }
+
+		if (e.keyCode === 9) {
+			e.preventDefault();
+			exec('insertHTML', false, '    ');
+		}
+	}
+
+	function insertTextAtCaret (text) {
+		var selection = window.getSelection();
+		var range;
+		var textNode;
+
+		if (selection.getRangeAt && selection.rangeCount) {
+			range = selection.getRangeAt(0);
+			range.deleteContents();
+			textNode = document.createTextNode(text);
+			range.insertNode(textNode);
+			selection.removeAllRanges();
+			range = range.cloneRange();
+			range.selectNode(textNode);
+			range.collapse(false);
+			selection.addRange(range);
+		}
+	}
+
+	function createEventBindings () {
+		var self = this;
+
+		document[addEventListener]('keyup', function (e) {
+			checkTextHighlighting.call(self, e);
+		});
+		document[addEventListener]('mousedown', function (e) {
+			checkTextHighlighting.call(self, e);
+		});
+		document[addEventListener]('mouseup', function(e) {
+			setTimeout( function() {
+				checkTextHighlighting.call(self, e);
+			});
+		});
+	};
+
+	function checkTextHighlighting (e) {
+		var self = this;
+
+		if (isChildOf(self.popover, e.target) ||
+			isChildOf(self.pickerContainer, e.target)) {
+			return; // allow the event to go through
+		}
+
+		var selection = window.getSelection();
+		self.saveSelection();
+
+		// text is selected
+		if (selection.isCollapsed === false) {
+			self.currentNodeList = findNodes(self.content, selection.focusNode);
+
+			// find if highlighting is in the editable area
+			if (hasNode(self.currentNodeList, 'PONYEDIT')) {
+				self.report();
+			}
+		}
+
+		wasCollapsed = selection.isCollapsed;
+
+		// report whether a selection exists
+		self.emit('report.active', !wasCollapsed);
+	};
+
+	function findNodes (content, element) {
+		var nodeNames = {};
+
+		while (element.parentNode) {
+			if (element === content ) {
+				nodeNames.PONYEDIT = true;
+			} else {
+				nodeNames[element.nodeName] = true;
+			}
+			element = element.parentNode;
+		}
+		return nodeNames;
+	};
+
+	function isChildOf ( parent, element ) {
+
+		while (element.parentNode) {
+			if (element === parent) {
+				return true;
+			}
+			if (element.parentNode === parent) {
+				return true;
+			}
+			element = element.parentNode;
+		}
+	};
+
+	function hasNode ( nodeList, name ) {
+		return !!nodeList[name];
+	};
+
+	Editor.prototype.focus = function () {
+		var range = document.createRange();
+		var selection = window.getSelection();
+		range.setStart(this.content, 0);
+		selection.removeAllRanges();
+		selection.addRange(range);
+	};
+
+	Editor.prototype.html = function (value) {
+		var self = this;
+
+		if (value === void 0) {
+			return self.content.innerHTML;
+		} else {
+			self.content.innerHTML = value;
+		}
+	};
+
+	Editor.prototype.saveSelection = function () {
+		var self = this, range;
+		var selection = window.getSelection();
+		if (selection.getRangeAt && selection.rangeCount) {
+			range = selection.getRangeAt(0);
+
+			// only update lastRange if selection is inside editor
+			if (isChildOf(self.content, range.commonAncestorContainer)) {
+				self.lastRange = range;
+			}
+		}
+	};
+
+	Editor.prototype.restoreSelection = function (forget) {
+		var self = this;
+		var selection;
+		var range = self.lastRange;
+		if (range) {
+			selection = window.getSelection();
+			selection.removeAllRanges();
+			selection.addRange(range);
+
+			if (forget === true) {
+				self.lastRange = null;
+			}
+		}
+	};
+
+	// contentEditable commands
+
+	function getSize () {
+		return parseInt(query('fontSize'), 10);
+	}
+
+	Editor.prototype.execBold = function () {
+		exec('bold', false);
+	};
+	Editor.prototype.execItalic = function () {
+		exec('italic', false);
+	};
+	Editor.prototype.execSize = function (value) {
+		exec('fontSize', false, value);
+	};
+	Editor.prototype.execSizeDecrease = function () {
+		exec('fontSize', false, getSize() - 1);
+	};
+	Editor.prototype.execSizeIncrease = function () {
+		exec('fontSize', false, getSize() + 1);
+	};
+	Editor.prototype.execType = function (value) {
+		exec('fontName', false, value);
+	};
+	Editor.prototype.execColor = function (value) {
+		exec('foreColor', false, value);
+	};
+	Editor.prototype.execAlignment = function (value) {
+		exec('justify' + value, false, null);
+	};
+
+	function command (action, args) {
+		return function () {
+			var self = this;
+			var args = Array.prototype.slice.call(arguments);
+			self.restoreSelection(true);
+			self['exec' + action].apply(self, args);
+			self['report' + action]();
+		};
+	}
+
+	Editor.prototype.setBold = command('Bold');
+	Editor.prototype.setItalic = command('Italic');
+	Editor.prototype.setSize = command('Size');
+	Editor.prototype.setType = command('Type');
+	Editor.prototype.setColor = command('Color');
+
+	// property state emission
+
+	function report (property, name, parse) {
+		var rquotes = /^['"]|['"]$/g;
+
+		return function () {
+			var self = this;
+			var value = query(property);
+			var ev = 'report.' + name;
+
+			if (parse === 'bool') {
+				value = value === 'true' || value === true;
+			} else if (parse === 'int') {
+				value = parseInt(value, 10);
+			} else if (property === 'fontName') {
+				value = value.replace(rquotes, '');
+			}
+
+			self.emit(ev, value);
+		};
+	}
+
+	Editor.prototype.reportBold = report('bold', 'bold', 'bool');
+	Editor.prototype.reportItalic = report('italic', 'italic', 'bool');
+	Editor.prototype.reportSize = report('fontSize', 'size', 'int');
+	Editor.prototype.reportType = report('fontName', 'type');
+	Editor.prototype.reportColor = report('foreColor', 'color');
+	Editor.prototype.report = function () {
+		var self = this;
+		self.reportBold();
+		self.reportItalic();
+		self.reportSize();
+		self.reportType();
+		self.reportColor();
+	};
+
+	Editor.prototype.meta = {
+		fontSizes: [1, 2, 3, 4, 5, 6, 7],
+		fontTypes: ['Arial', 'Arial Black', 'Comic Sans MS', 'Courier', 'Courier New', 'Georgia', 'Helvetica', 'Impact', 'Palatino', 'Times New Roman', 'Trebuchet MS', 'Verdana'],
+		alignments: ['Left', 'Center', 'Right']
+	};
+
+	// lock down the meta options
+	Object.freeze(Editor.prototype.meta);
+
+	window.ponyedit = function (element) {
+		return find(element);
+	};
+
+	window.ponyedit.meta = Editor.prototype.meta;
+	window.ponyedit.init = function (element, options) {
+		if (element instanceof Editor) {
+			return element;
+		}
+
+		var instance = find(element);
+		if (instance) {
+			return instance;
+		}
+		return new Editor(element, options);
+	};
+
+})(window, document, EventEmitter2);
