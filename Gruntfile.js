dca6a0a0127f09c93a16e0a4ccfec35c6b94242e
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2013-11-08 10:59:12 -0300

    load grunt tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -84,14 +84,9 @@ module.exports = function (grunt) {
             }
         }
     });
-    grunt.loadNpmTasks('grunt-bump');
-    grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-contrib-csslint');
-    grunt.loadNpmTasks('grunt-contrib-cssmin');
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    require('load-grunt-tasks')(grunt);
+
     grunt.registerTask('lint', ['jshint', 'csslint']);
     grunt.registerTask('default', ['lint']);
     grunt.registerTask('build', ['lint', 'clean', 'uglify', 'cssmin', 'copy']);
