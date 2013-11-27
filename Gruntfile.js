9b03e0bc404591b28628bef736dedfc906b16633
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2013-11-27 14:37:57 -0300

    updating build

--- Gruntfile.js
+++ Gruntfile.js
@@ -29,6 +29,9 @@ module.exports = function (grunt) {
                 files: {
                     'web/statics/js/eventemitter2.min.js': [
                         'bower_components/eventemitter2/lib/eventemitter2.js'
+                    ],
+                    'web/statics/js/rangy.js': [
+                        'bower_components/rangy/rangy-core.js'
                     ]
                 }
             }
